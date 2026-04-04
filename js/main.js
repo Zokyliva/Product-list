5244002d6973be7ae8e84aec38a32aa3a1b64f49
Zokyliva <heriniainao807@gmail.com>
2026-04-05 01:38:55 +0300

    Using Tailwind V4

--- js/main.js
+++ js/main.js
@@ -2,11 +2,16 @@ import { Cart } from "./models/Cart.js";
 import { ProductView } from "./views/ProductViews.js";
 import { CartView } from "./views/CartViews.js";
 import { appController } from "./controllers/appController.js";
+import { Product , dataDessert } from "./models/Product.js";
 
 const cart = new Cart();
 const productView = new ProductView(document.querySelector('.tous-items'));
 const cartView = new CartView();
 
-const app = new appController(cart , cartView , productView);
+const product = dataDessert.map(({id , name , category , image , price}) => {
+    return new Product({id , name , category , image , price});
+})
+
+const app = new appController(cart , cartView , productView , product);
 
 app.init();
